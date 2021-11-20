ae12ed8a54143cb6ccdd3497e36c27e615f04b53
Anderson <BlackCode7@outlook.com>
2021-11-19 22:21:18 -0300

    Add path bean

--- API_Java/venda/src/main/java/io/blackcode/VendasApplication.java
+++ API_Java/venda/src/main/java/io/blackcode/VendasApplication.java
@@ -1,10 +1,29 @@
 package io.blackcode;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class VendasApplication {
+
+    // https://github.com/cursodsousa/curso-spring-boot-especialista/tree/03_configuracoes_e_beans
+    // https://github.com/cursodsousa/curso-spring-boot-especialista/tree/03_configuracoes_e_beans
+    // Link para comparação de código
+
+    @Autowired
+    @Qualifier("applicationName")
+    private String applicationName;
+
+    @GetMapping("/hello")
+    public String helloWorld(){
+        return applicationName;
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class, args);
     }
