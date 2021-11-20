5329fd013e3d197ba75e696cee7e6f47e7b3add9
Anderson <BlackCode7@outlook.com>
2021-11-20 15:54:55 -0300

    Avançando na leitura do Clean Code

---
+++ LivroCleanCode/src/Chapter_3/PageData.java
@@ -0,0 +1,20 @@
+package Chapter_3;
+
+public class PageData<WikiPage> {
+
+	public WikiPage getWikiPage() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Chapter_3.WikiPage WikiPage() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public boolean hasAttribute(String string) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
---
+++ LivroCleanCode/src/Chapter_3/WikiPage.java
@@ -0,0 +1,10 @@
+package Chapter_3;
+
+public class WikiPage {
+
+	public Object getPageCrawler() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
